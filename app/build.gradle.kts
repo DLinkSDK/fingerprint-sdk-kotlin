8d112ca9ffcbccbe632ba9e6632479cf23d8d504
ace1946 <ace@dlink.cloud>
2025-03-31 11:45:23 +0800

    Update SDK to 3.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
 
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.id)
+    implementation(libs.fingerprint)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
